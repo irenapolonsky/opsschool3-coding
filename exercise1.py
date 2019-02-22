93badc1e2eafe1ac1104796590e0c8861d916732
irena.polonsky <polonsky.irena@gmail.com>
2019-02-22 20:22:17 +0200

    input file and code

--- exercise1.py
+++ exercise1.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python
 import json
 import operator
-#import yaml
-ex1 = open('Ex1_input','r',encoding='utf-8')
-json_string = ex1.read()
+import yaml
+
+exercise1 = open('exercise1_input','r',encoding='utf-8')
+json_string = exercise1.read()
 
 datastore = json.loads(json_string)
-#print(datastore)
 ages_dict = datastore ["ppl_ages"]
 buckets_list = datastore ['buckets']
-#print(ages_dict['Dan'])
-#print(buckets_list)
-
 
 max_age = max(ages_dict.items(), key=operator.itemgetter(1))[1]
 max_age += 1
-#print(max_age)
 buckets_list+=[0,max_age]
-
 buckets_list.sort()
 buckets_counter = len(buckets_list)
-#print(buckets_list,buckets_counter)
 
-fo = open('tempfile','w')
+keystring = "{0} - {1}"
+result = {}
+
 for i in range(1,buckets_counter):
-#    line1 = buckets_list[i-1]+'-'+ buckets_list[i]
-    print(buckets_list[i-1],'-',buckets_list[i],':')
-#    fo.write(line1)
-    for key in ages_dict:
-        if buckets_list[i-1] < ages_dict[key]<buckets_list[i]:
-#            line2 = '- :'+key+ages_dict[key]
-            print('- :',key,ages_dict[key])
+    val = [""]
+    dictkey = keystring.format(buckets_list[i-1],buckets_list[i])
+    for name in ages_dict:
+        if buckets_list[i-1] < ages_dict[name]<buckets_list[i]:
+            val.append(':'+name)
+    result[dictkey] = val
 
-fo.close()
+with open('result', 'w') as yaml_file:
+    yaml.dump(result, yaml_file, default_flow_style=False,encoding='utf-8')
+
+
+
+
 
